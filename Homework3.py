5feca45e24a84f2fc00998a738d3e71ee3775be8
Oleg <Oleg.evpator@mail.ru>
2023-01-17 13:51:53 +0300

    add Homework3

--- Homework3.py
+++ Homework3.py
@@ -1,3 +1,5 @@
+# Задача 1. Создайте файл. Запишите в него N первых
+# элементов последовательности Фибоначчи.
 def zadacha1():
     n = int(input('Введите длину ряда: '))
     f1 = 0
@@ -8,28 +10,37 @@ def zadacha1():
         data.write(str(f1) + '\n')
         data.close
         print(f2, end=' ')
-    
+   
 zadacha1()
 # Задача 2. В файле находятся названия фруктов.
 # Выведите все фрукты, названия которых начинаются на
 # заданную букву.
-# let = input('Введите первую букву названия фрукта: ')
-# data = open('fruits.txt', mode='r', encoding='utf-8')
-# text = data.readlines()
-# data.close
-# first_liter = ''
-# count = 0
-# for let in text:
-#     if let == text[0]:
-#         first_liter = text
-#         print(first_liter)
-        
-        
-# phrase = input(  )
-# dictionary = {
-#             'Привет': 'Здравствуйте',
-#             'Как тебя зовут': 'Олег'
-# }
-# if phrase == dictionary[phrase]:
-#     print(dictionary['Привет'])
+def zadacha2():
+    let = input('Введите первую букву названия фрукта: ')
+    data = open('fruits.txt', encoding='utf-8')
+    Fruits_lst = data.readlines()[0].split(' ')
+    data.close
+    for fruit in Fruits_lst:
+        if fruit[0] == let:
+            print(fruit)
+zadacha2()        
+#  Задача 3. Создайте скрипт бота, который находит ответы
+# на фразы по ключу в словаре. Бот должен, как минимум,
+# отвечать на фразы «привет», «как тебя зовут». Если фраза
+# ему неизвестна, он выводит соответствующую фразу.       
+def zadacha3():
+    dictionary = {
+            'привет': 'Здравствуйте',
+            'как тебя зовут?': 'Олегсей'
+}
+    dia = True
+    while dia:
+        phrase = input('Я ')
+        if phrase in dictionary.keys():
+            print(f'Bot: {dictionary[phrase]}')
+        else:
+            print(f'Bot: не понимать')
+        if phrase == 'пока':
+            dia = False
+zadacha3()
 
